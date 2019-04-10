e7f0d5e1b4e3b3631b3e0ca5257abbc90f52bac1
brianlucas1 <37352043+brianlucas1@users.noreply.github.com>
2019-04-10 15:50:14 -0300

    Update Alfabeto.java

--- Alfabeto em Z (Matrizes)/Alfabeto.java
+++ Alfabeto em Z (Matrizes)/Alfabeto.java
@@ -7,29 +7,33 @@ public class Alfabeto {
 
 public static void main(String[] args) {
 	
-	  /// Declarando as variaveis
-    Scanner scan = new Scanner(System.in); //Metodo Scanner, onde o codigo lê os valores digitados.
+	
+    Scanner scan = new Scanner(System.in); 
  
+	//  STRING ALFABETO RECEBE O ABECEDARIO,
     String alfabeto = "abcdefghijklmnopqrstuxwyz";
-    char vletra[] = alfabeto.toCharArray(); //Declarando onde o vetor recebe as letras
+	// ATRIBUINDO CADA LETRA DO ALFABETO EM UMA MATRIZ
+    char vletra[] = alfabeto.toCharArray(); 
     System.out.println("LINHAS  ");
     int mLinha = scan.nextInt();
     System.out.println("COLUNAS   ");
     int mColuna = scan.nextInt();
+	// CONTADOR DAS LETRAS DO ALFABETO PARA QUE NÃO ULTRAPASSEM 26, POIS O ALFABETO TEM 26 LETRAS.
     int contAlfabeto=0,
     	contMaLinha=1,
+	// FAZ A FORMA DO "Z" 
     	contMaColuna=mColuna-2;
 
     
 
-// atribuindo as posições da matriz
+// Contato os espaços
     char[][] matriz = new char[mLinha][mColuna];
     for(int i=0;i<mLinha;i++){
         for(int j=0;j<mLinha;j++){
         	matriz[i][j]=' ';
         }
     }
-    //  duas primeiras linhas são inteiras são sempre preenchidas e add uma letra 	por linha
+    //   Atribuindo a coluna uma letra do alfabeto 
     for(int i=0;i<mColuna;i++){
     	matriz[0][i]=vletra[contAlfabeto];
         contAlfabeto++;
@@ -38,7 +42,7 @@ public static void main(String[] args) {
         }
     }
     
-    //// contador onde a ultima letra da linha diminui o da coluna formando o z
+    //// 
     for(;(contMaLinha+1)<mLinha;contMaLinha++,contMaColuna--){
     	matriz[contMaLinha][contMaColuna]=vletra[contAlfabeto];
         contAlfabeto++;
@@ -52,11 +56,11 @@ public static void main(String[] args) {
     for(int i=0;i<mLinha;i++){
     	matriz[contMaLinha][i]=vletra[contAlfabeto];
         contAlfabeto++;
-        // // se chegar na ultima letra do alfabeto volta para o A
+    
         if(contAlfabeto==26){
             contAlfabeto=0;
         }
-    }//saida da matriz
+    }
     for(int i=0;i<mLinha;i++){
         for(int j=0;j<mColuna;j++){
             System.out.print(matriz[i][j]+"   ");
