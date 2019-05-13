3b0b360ff5bf07e51f0809516f82886333f017e8
brianlucas1 <37352043+brianlucas1@users.noreply.github.com>
2019-05-13 10:03:50 -0300

    APRENDENDO - WEB - MVC

---
+++ AreaQuadMVC/src/java/modelo/Calculo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author bcamargo
+ */
+public class Calculo {
+    
+   public static float areaQuadrado(float largura, float comprimento){
+       return largura*comprimento;
+          }
+}
+   
+
